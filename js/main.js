168c95d30ea764eee55f17a74ffb9b1f114eef6a
JohnnyPikoneron <dj_serje@inbox.ru>
2017-05-14 16:29:42 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+﻿
+    $("#menu").mmenu({
+        "slidingSubmenus": false,
+        "extensions": [
+            "pagedim-black",
+            "theme-dark"
+        ],
+        "offCanvas": {
+            "position": "right"
+        }
+    });
+
+
+    $( function() {
+        $( "#slider-range-min" ).slider({
+            range: "min",
+            value: 357,
+            min: 1,
+            max: 700,
+            slide: function( event, ui ) {
+                $( "#amount" ).val( "$" + ui.value );
+            }
+        });
+        $( "#amount" ).val( "$" + $( "#slider-range-min" ).slider( "value" ) );
+    } );
+
+
